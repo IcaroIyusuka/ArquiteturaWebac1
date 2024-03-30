87f3823b14e4c86a3cc203742e23c27603aaff68
IcaroIyusuka <icaroiyusuka@gmail.com>
2024-03-30 15:56:31 -0300

    Parte3

--- Ac1/src/main/java/me/icaro/Ac1/repository/AlunoRepositoryImpl.java
+++ Ac1/src/main/java/me/icaro/Ac1/repository/AlunoRepositoryImpl.java
@@ -2,40 +2,62 @@ package me.icaro.Ac1.repository;
 
 import me.icaro.Ac1.modelo.Aluno;
 import org.springframework.stereotype.Repository;
-import java.util.ArrayList;
+import org.springframework.jdbc.core.JdbcTemplate;
 import java.util.List;
 
 @Repository
 public class AlunoRepositoryImpl implements AlunoRepository {
-    private final List<Aluno> alunos = new ArrayList<>();
-    private Long nextId = 1L;
 
-    public AlunoRepositoryImpl(){
-    alunos.add(new Aluno(1L,"Jorge","M","Alooobrasil@gmail.com","78961233"));
-    nextId = 2L;
+    private final JdbcTemplate jdbcTemplate;
+
+    public AlunoRepositoryImpl(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
     }
+
     @Override
     public List<Aluno> findAll(){
-        return alunos;
+
+        return jdbcTemplate.query("SELECT * FROM aluno", (resultSet, rowNum) ->{
+            System.out.println("Numero da linha: " + rowNum);
+            return new Aluno(
+                resultSet.getLong("id"),
+                resultSet.getString("nome"),
+                resultSet.getString("sexo"),
+                resultSet.getString("email"),
+                resultSet.getString("telefone")
+            );
+        });
     }
 
     @Override
-    public Aluno findById(Long id){
-        return alunos.stream()
-                .filter(aluno -> aluno.getId().equals(id))
-                .findFirst()
-                .orElse(null);
+    public Aluno findById (Long id){
+        String query = "SELECT * FROM aluno where id = ?";
+
+        return jdbcTemplate.queryForObject(query, new Object[]{id}, (resultSet, rowNum) ->
+                new Aluno
+                        (
+                        resultSet.getLong("id"),
+                        resultSet.getString("nome"),
+                        resultSet.getString("sexo"),
+                        resultSet.getString("email"),
+                        resultSet.getString("telefone")
+                        )
+                );
     }
 
     @Override
-    public Aluno save(Aluno aluno) {
-        if (aluno.getId()==null){
-            aluno.setId(nextId++);
-            alunos.add(aluno);
-        } else {
-            alunos.removeIf(t -> t.getId().equals(aluno.getId()));
-            alunos.add(aluno);
+    public Aluno save(Aluno aluno){
+        if (aluno.getId() != null){
+            String insertQuery = "INSERT INTO public.aluno (id, nome, sexo, email, telefone) VALUES (?,?,?,?,?)";
+
+            jdbcTemplate.update(insertQuery, aluno.getId(), aluno.getNome(), aluno.getSexo(), aluno.getEmail(), aluno.getTelefone());
+
+        } else{
+            String updateQuery = "UPDATE public.aluno SET nome = ?, sexo = ?, email = ?, telefone = ? WHERE id = ?";
+            jdbcTemplate.update(updateQuery, aluno.getNome(), aluno.getSexo(), aluno.getEmail(), aluno.getTelefone(), aluno.getId());
         }
         return aluno;
+
     }
+
 }
---
+++ Ac1/src/main/resources/db/database.sql
@@ -0,0 +1,29 @@
+-- Database: alunos
+
+-- DROP DATABASE IF EXISTS alunos;
+
+CREATE DATABASE alunos
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    LOCALE_PROVIDER = 'libc'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+-- Criação da tabela 'task'
+CREATE TABLE IF NOT EXISTS public.aluno
+(
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    sexo VARCHAR(10),
+    email VARCHAR(255),
+    telefone VARCHAR(20)
+);
+
+-- Altera o dono da tabela 'task'
+ALTER TABLE IF EXISTS public.aluno
+    OWNER TO icaro;
+
